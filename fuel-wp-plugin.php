4e544eb2c32f885d7523d86176f9de6bfa433ed6
djsmale <binaryfrost@gmail.com>
2021-07-31 11:52:32 +0100

    WIP

--- fuel-wp-plugin.php
+++ fuel-wp-plugin.php
@@ -22,3 +22,22 @@ function fuelwpplugin_activate() {
 }
 
 register_activation_hook( __FILE__, 'fuelwpplugin_activate' );
+
+function fuelwpplugin_options_page_html() {
+    ?>
+    <div class="wrap">
+      <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+      <p>Some paragraph.</p>
+    </div>
+    <?php
+}
+
+add_menu_page(
+    'Fuel Plugin', // $page_title
+    'Fuel Plugin', //$menu_title
+    'manage_options', // $capability
+    'fuelplugin', // $menu_slug
+    'fuelwpplugin_options_page_html',
+    'dashicons-admin-generic', // $icon_url
+    null
+);
