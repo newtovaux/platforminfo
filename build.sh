653d68b39348277a01693a1c1a7b2c10e9630d2d
djsmale <binaryfrost@gmail.com>
2021-12-28 09:09:34 +0000

    Tidied up build script

--- build.sh
+++ build.sh
@@ -3,5 +3,13 @@
 rm platform.zip
 
 pushd ..
-zip -9 platform/platform.zip -r platform/ -x 'platform/*.git*' -x 'platform/*.zip' -x 'platform/build.sh' -x 'platform/vendor/*' -x 'platform/composer.*' -x 'platform/*.xml'
+
+zip -9 platform/platform.zip -r platform/ \
+    -x 'platform/*.git*' \
+    -x 'platform/*.zip' \
+    -x 'platform/build.sh' \
+    -x 'platform/vendor/*' \
+    -x 'platform/composer.*' \
+    -x 'platform/*.xml'
+
 popd
