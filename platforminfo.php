e6a7ea3fd27e71fa73b54a67a71356b61366776d
newtovaux <newtovaux>
2026-09-10 08:33:08 +0100

    Release 1.3.1 for WP 7.1

--- platforminfo.php
+++ platforminfo.php
@@ -3,7 +3,7 @@
  * Plugin Name:       PlatformInfo
  * Plugin URI:        https://en-gb.wordpress.org/plugins/platforminfo/
  * Description:       PlatformInfo gives useful information for sites on shared hosting.
- * Version:           1.3.0
+ * Version:           1.3.1
  * Requires at least: 5.8
  * Requires PHP:      7.4
  * Author:            newtovaux
